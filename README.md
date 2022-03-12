561ac6c608119ce92329da137fd08a1f74f27edc
Naoto Ikeno <ikenox@gmail.com>
2022-03-12 14:14:52 +0900

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# ftx-history-downloader
+
+A script to download FTX execution history of a specific FTX account.
+
+## Disclaimer
+
+This script is not tested well. Please use at your own risk.
+
+## Usage
+
+```shell
+# Before doing, please make a credential json file like the following format
+# It's recommended to use an API key which has only read permission
+$ cat ./credential.json
+> {"api_key": "XXX", "api_secret": "XXX"}
+
+# Execute
+$ RUST_LOG=info cargo run --release -- \
+    --outdir ./output \
+    --credential ./credential.json \
+    # You can omit this option if you want to obtain main account's history
+    --sub-account sub1
+    
+# The collected history will be saved to the specified output directory
+$ ls ./output
+> sub1-2020-11-21.tsv sub1-2020-12-10.tsv sub1-2021-10-11.tsv sub1-2021-11-21.tsv
+  sub1-2020-11-22.tsv sub1-2020-12-20.tsv sub1-2021-10-13.tsv sub1-2021-11-24.tsv
+  sub1-2020-11-29.tsv sub1-2021-04-03.tsv sub1-2021-11-10.tsv sub1-2021-11-25.tsv 
+  ...
+```
+
+## License
+
+MIT License
+
+Copyright (c) 2022 Naoto Ikeno
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/main.rs
@@ -0,0 +1,233 @@
+use std::path::PathBuf;
+use std::time::SystemTime;
+
+use anyhow::{Context, Error, Result};
+use chrono::{DateTime, Local, NaiveDate, NaiveDateTime, Utc};
+use clap::Parser;
+use csv_async::AsyncSerializer;
+use futures::StreamExt;
+use futures::{FutureExt, TryFutureExt};
+use log::*;
+use serde::{Deserialize, Serialize};
+use surf::Request;
+use tokio::fs::File;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(long, parse(from_os_str))]
+    credential: PathBuf,
+    #[clap(long, parse(from_os_str))]
+    outdir: PathBuf,
+    #[clap(long)]
+    sub_account: Option<String>,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let args: Args = Args::parse();
+
+    let cred: FtxCredential = serde_json::from_str(
+        &tokio::fs::read_to_string(args.credential)
+            .await
+            .expect("failed to read credential file"),
+    )
+    .expect("failed to parse credential file");
+
+    let outdir = &args.outdir;
+    let sub_account = &args.sub_account;
+    futures::stream::unfold(
+        RequestCursor {
+            end_time: Utc::now().naive_utc(),
+            oldest_fill_id: u64::MAX,
+        },
+        |RequestCursor {
+             end_time,
+             oldest_fill_id,
+         }| {
+            // FTX API returns up to 5000 fills order by time desc
+            // So always specifying start_time=zero and moves end_time to obtain all fills
+            get_fills(0, end_time.timestamp(), &cred, sub_account).map(move |result| {
+                let fills = result
+                    .expect("failed to request")
+                    .into_iter()
+                    // avoid duplication
+                    .filter(|f| f.id < oldest_fill_id)
+                    .collect::<Vec<_>>();
+                let next_cursor = fills.last().map(|oldest| {
+                    info!(
+                        "{} fills between {} and {}",
+                        fills.len(),
+                        oldest.time.timestamp(),
+                        end_time.timestamp()
+                    );
+                    RequestCursor {
+                        end_time: oldest.time.naive_utc(),
+                        oldest_fill_id: oldest.id,
+                    }
+                });
+
+                next_cursor.map(|c| (fills, c))
+            })
+        },
+    )
+    .flat_map(|fills| futures::stream::iter(fills))
+    .fold(None, |cursor, fill: FtxFill| async move {
+        let fill_date = fill.time.date().naive_utc();
+        let mut writer = cursor
+            .and_then(
+                |WriterCursor {
+                     target_date: date,
+                     writer,
+                 }| {
+                    if date == fill_date {
+                        Some(futures::future::ready(writer).left_future())
+                    } else {
+                        // date is changed so change file to write
+                        None
+                    }
+                },
+            )
+            .unwrap_or_else(|| {
+                // new writer
+                let filepath = outdir.join(format!(
+                    "{}_{}.tsv",
+                    sub_account
+                        .as_ref()
+                        .map(|s| s.as_str())
+                        .unwrap_or_else(|| "main"),
+                    fill_date,
+                ));
+                tokio::fs::create_dir_all(outdir)
+                    .then(|result| {
+                        result.expect("failed to create directories");
+                        File::create(filepath)
+                    })
+                    .map(|result| {
+                        csv_async::AsyncSerializer::from_writer(
+                            result.expect("failed to open file to write"),
+                        )
+                    })
+                    .right_future()
+            })
+            .await;
+        writer
+            .serialize(&fill)
+            .await
+            .expect("failed to write data to file");
+        Some(WriterCursor {
+            target_date: fill_date,
+            writer,
+        })
+    })
+    .await;
+}
+
+async fn get_fills(
+    start_time: i64,
+    end_time: i64,
+    credential: &FtxCredential,
+    sub_account: &Option<String>,
+) -> Result<Vec<FtxFill>> {
+    let response_body = surf::client()
+        .send(signed_request(
+            surf::get(format!(
+                "https://ftx.com/api/fills?start_time={}&end_time={}",
+                start_time, end_time
+            ))
+            .build(),
+            credential,
+            sub_account,
+        ))
+        .await
+        .map_err(Error::msg)?
+        .body_string()
+        .await
+        .map_err(Error::msg)?;
+
+    serde_json::from_str::<FtxResponse<_>>(&response_body)
+        .with_context(|| {
+            format!(
+                "unexpected response json format. \n\nresponse body:\n{}",
+                response_body
+            )
+        })
+        .map(|body_json| body_json.result)
+}
+
+fn signed_request(
+    mut rb: Request,
+    credential: &FtxCredential,
+    sub_account: &Option<String>,
+) -> Request {
+    let ts = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64;
+    let text = format!(
+        "{}{}{}{}",
+        ts,
+        rb.method().to_string(),
+        rb.url().path(),
+        rb.url()
+            .query()
+            .map(|q| format!("?{q}"))
+            .unwrap_or_else(|| "".to_string())
+    );
+    rb.set_header("FTX-KEY", &credential.api_key);
+    rb.set_header("FTX-TS", ts.to_string());
+    rb.set_header(
+        "FTX-SIGN",
+        hex::encode(hmac_sha256::HMAC::mac(&text, &credential.api_secret)),
+    );
+    if let Some(sub_account) = &sub_account {
+        rb.set_header("FTX-SUBACCOUNT", sub_account);
+    }
+
+    rb
+}
+
+struct RequestCursor {
+    end_time: NaiveDateTime,
+    oldest_fill_id: u64,
+}
+
+struct WriterCursor {
+    target_date: NaiveDate,
+    writer: AsyncSerializer<File>,
+}
+
+#[derive(Deserialize)]
+pub struct FtxCredential {
+    pub api_key: String,
+    pub api_secret: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FtxResponse<T> {
+    result: T,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FtxFill {
+    pub fee: f64,
+    pub fee_currency: Option<String>,
+    pub fee_rate: Option<f64>,
+    pub future: Option<String>,
+    pub id: u64,
+    pub liquidity: Option<String>,
+    pub market: Option<String>,
+    pub base_currency: Option<String>,
+    pub quote_currency: Option<String>,
+    pub order_id: Option<u64>,
+    pub trade_id: Option<u64>,
+    pub price: f64,
+    pub side: Option<String>,
+    pub size: f64,
+    pub time: DateTime<Local>,
+    #[serde(rename = "type")]
+    pub typ: Option<String>,
+}
